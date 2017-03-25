b43ed9aa2e175aac97da66e06b8d8847d5d16fd2
Fabio <fabio.queluci@gmail.com>
2017-03-25 19:00:35 -0300

    Allow users to sign up.

--- imports/ui/auth/signUp.js
+++ imports/ui/auth/signUp.js
@@ -1,1 +1,16 @@
+import { Template } from 'meteor/templating';
 import './signUp.html';
+
+Template.signUp.events({
+  'submit form': function(e){
+    e.preventDefault();
+
+    var email = $('[name=email]').val();
+    var password = $('[name=password]').val();
+
+    Accounts.createUser({
+      email: email,
+      password: password
+    });
+  }
+});
