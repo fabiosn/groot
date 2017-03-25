06375926a684b44cb5bf3db1b6d086d94158dd72
Fabio <fabio.queluci@gmail.com>
2017-03-25 18:07:47 -0300

    Create the basic routes and templates for the landing page and the auth pages

--- client/main.js
+++ client/main.js
@@ -1,1 +1,18 @@
+import { Router } from 'meteor/iron:router';
 import '../imports/ui/body.js';
+import '../imports/ui/landingPageTemplate.js';
+import '../imports/ui/auth/signUp.js';
+import '../imports/ui/auth/signIn.js';
+
+
+Router.route('/', function () {
+  this.render('LandingPageTemplate');
+});
+
+Router.route('/sign_up', function () {
+  this.render('signUp');
+});
+
+Router.route('/sign_in', function () {
+  this.render('signIn');
+});
--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,1 +1,2 @@
 import './body.html';
+import './landingPageTemplate.js'
